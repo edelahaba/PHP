67ea90fabd1f36661f787bf57de0e906ceca0d65
edelahaba <ernescorh93@gmail.com>
2015-12-07 17:00:55 +0100

    Version preliminar de la visualizacion de cuentas

--- Banco/gestion/visualizarClientes.php
+++ Banco/gestion/visualizarClientes.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Nesto
- * Date: 07/12/2015
- * Time: 12:28
- */
+spl_autoload_register(function ($clase) {
+    include '../clases/' . $clase . '.php';
+});
+
+session_start();
+
+//Crea una session con clientes y si no existe te lo crea
+if (isset($_SESSION['clientes'])) {
+    $clientes = $_SESSION['clientes'];
+    // echo var_dump($clientes);
+    echo "<table border='1'>";
+    echo "<tr><td>Nombre cliente</td><td>Id de cliente</td></tr>";
+    for ($i = 0; $i < count($clientes); $i++) {
+        echo "<tr><td>" . $clientes[$i]-> getNombre() . "</td><td>" . $clientes[$i]-> getNumTarjeta() . "</td>";
+    }
+    echo "</table>";
+} else {
+    echo "No hay clientes registrados";
+    header("refresh:2;url=../index.php");
+}
+?>
