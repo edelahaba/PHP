013128dbdc66d0ac9d6ada68a62026a1d15613b0
edelahaba <ernescorh93@gmail.com>
2015-12-07 10:24:36 +0100

    Se añade el soporte para sessiones y un menu para poder acceder a las diferentes partes del proyecto

--- Banco/index.php
+++ Banco/index.php
@@ -1,6 +1,17 @@
 <?php
 session_start();
+//Crea una session con clientes y si no existe te lo crea
 if(!isset($_SESSION['clientes'])){
     $_SESSION['clientes'] = Array();
 }
 ?>
+<html>
+<a href="crearClientes">Crear clientes</a>
+<br>
+<a href="visualizarClientes">Visualizar clientes</a>
+<br>
+<a href="crearCuentas">Crear cuentas</a>
+<br>
+<a href="visualizarCuentas">Crear cuentas</a>
+</html>
+
